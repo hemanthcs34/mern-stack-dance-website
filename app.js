d0eac4697b0e4aad4ede6c3f7940552b25ba8f44
hemanthcs34 <cshemanthcs@gmail.com>
2025-08-01 21:59:32 +0530

    a fullstack dance website using mern stack ft:working on frontend

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8000;
+//express related stuff
+app.use(express.static('static'))//for static files
+app.use(express.urlencoded())
+
+//pug related stuff
+app.set('view engine','pug')//set the template engine as pug)
+app.set('views',path.join(__dirname,'views'))
+
+//endpoints
+app.get('/',(req,res)=>{
+    const params = {}
+    res.status(200).render('index.pug',params);
+})
+
+//server start
+app.listen(port,()=>{
+    console.log(`The application started successfully on port ${port}`);
+})
