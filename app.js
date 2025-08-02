c9f4b1c5fd88d63f731dc943fa9447eab47a407b
hemanthcs34 <cshemanthcs@gmail.com>
2025-08-02 22:50:59 +0530

    preparing this website using pug

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ app.set('views',path.join(__dirname,'views'))
 //endpoints
 app.get('/',(req,res)=>{
     const params = {}
-    res.status(200).render('index.pug',params);
+    res.status(200).render('home.pug',params);
+})
+app.get('/contact',(req,res)=>{
+    const params = {}
+    res.status(200).render('contact.pug',params);
 })
 
 //server start
